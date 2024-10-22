c2f5e4669a55e47a1e1f790930445ea79fafa9d1
Ville Soerensen <viso1@elev.aatg.dk>
2024-10-22 11:50:21 +0000

    NVIM: changed bufferline config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,7 +2,7 @@ require("config.lazy")
 require("config.keymaps")
 require("config.nvim-cmp")
 require("config.heirline")
+require("config.bufferline")
 
 vim.cmd.colorscheme "catppuccin"
 vim.opt.termguicolors = true
-require("bufferline").setup{}
