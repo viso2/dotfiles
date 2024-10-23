c866df4bff75cf0b2498a212be62e1d62b6204ab
Ville Soerensen <viso1@elev.aatg.dk>
2024-10-23 08:46:12 +0000

    NVIM: updated init.lua to include the config for gitsigns

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,6 +3,12 @@ require("config.keymaps")
 require("config.nvim-cmp")
 require("config.heirline")
 require("config.bufferline")
+require("config.gitsigns")
 
 vim.cmd.colorscheme "catppuccin"
 vim.opt.termguicolors = true
+vim.o.relativenumber = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.wrap = false
