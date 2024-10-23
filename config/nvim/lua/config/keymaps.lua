30e4fbdd50c53f1a0d9e1f6e20ac411e5fe67c8f
Ville Soerensen <viso1@elev.aatg.dk>
2024-10-23 08:44:12 +0000

    NVIM: updated some keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -6,9 +6,9 @@ vim.keymap.set("n", "<Leader>ff", require("telescope.builtin").find_files, { des
 vim.keymap.set("n", "<Leader>fg", require("telescope.builtin").live_grep, { desc = "Telescope find files "})
 
 -- neo-tree
-vim.keymap.set("n", "<Leader>e", "<Cmd>Neotree<CR>", { desc = "Toggle Explorer"})
+vim.keymap.set("n", "<Leader>e", "<Cmd>Neotree<CR>", { desc = "Enable Explorer"})
 vim.keymap.set("n", "<Leader>o", ":lua FocusNeoTree()<CR>", { desc = "Changes the focus to/from neotree" })
-
+vim.keymap.set("n", "<Leader>c", "<Cmd>Neotree close<CR>", { desc = "Close Explorer"})
 
 function FocusNeoTree()
 	local filetype = vim.bo.filetype
